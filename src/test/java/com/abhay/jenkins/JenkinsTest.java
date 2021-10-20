a73f43d678713b325b558f9ffcc518c7fea60b4c
Abhay Kumar <74773868+abhay72@users.noreply.github.com>
2021-10-20 13:48:56 +0530

    Update JenkinsTest.java

--- src/test/java/com/abhay/jenkins/JenkinsTest.java
+++ src/test/java/com/abhay/jenkins/JenkinsTest.java
@@ -7,12 +7,12 @@ import org.junit.Test;
 public class JenkinsTest {
 
 	@Test
-	public void addTest() {
+	public int addTest() {
 		JenkinsCalculator myCalc = new JenkinsCalculator();
 		assertEquals(10,myCalc.add_num(5,5));
 	}
 	@Test
-	public void subtractTest() {
+	public int subtractTest() {
 		JenkinsCalculator myCalc = new JenkinsCalculator();
 		assertEquals(5,myCalc.sub_num(10,5));
 	}
